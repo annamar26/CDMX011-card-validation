e502785b199ca6bc5e503469b9d3ed32b2f52687
Ana Aguilar <anaaguilar@Mac-mini-de-Ana.local>
2022-01-06 23:51:36 -0600

    add styles

--- src/validator.js
+++ src/validator.js
@@ -1,34 +1,40 @@
 const validator = {
-   maskify(cardNumber) {
+  maskify(cardNumber) {
     if (cardNumber.length > 4) {
-      return cardNumber.replace( cardNumber.substring(0, cardNumber.length - 4), "#".repeat(cardNumber.length - 4)
+      return cardNumber.replace(
+        cardNumber.substring(0, cardNumber.length - 4),
+        "#".repeat(cardNumber.length - 4)
       );
     }
     return cardNumber;
   },
 
   isValid(cardNumber) {
-    const array = []
-  for (let i = 0; i < cardNumber.length; i++) {
-if(i%2!==0){
- const double= cardNumber[i]*2;
+    const array = [];
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i % 2 !== 0) {
+        const double = cardNumber[i] * 2;
 
- if(double.toString().length==2){
-   const digits = double.toString().split('')
-   array.push(digits.reduce((accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue)))
- }else{
-   array.push(double)
- }
-  
- 
-}else{
-  array.push(parseInt(cardNumber[i]))
-}
+        if (double.toString().length == 2) {
+          const digits = double.toString().split("");
+          array.push(
+            digits.reduce(
+              (accumulator, currentValue) =>
+                parseInt(accumulator) + parseInt(currentValue)
+            )
+          );
+        } else {
+          array.push(double);
+        }
+      } else {
+        array.push(parseInt(cardNumber[i]));
+      }
     }
-    const result = array.reduce((accumulator, currentValue)=> accumulator+ currentValue)
-   return result%10===0? true : false
+    const result = array.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    return result % 10 === 0 ? true : false;
   },
-  
 
   getIssuer(creditCardNumber) {
     let emisora;
